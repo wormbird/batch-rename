e81aa24a264f113a87d40de5d729ba965f95b74d
weifeng <wind91@foxmail.com>
2019-07-18 13:36:24 +0800

    update

--- rename.py
+++ rename.py
@@ -17,9 +17,9 @@ def batch_rename(path, name):
     name_type = os.path.splitext(name)[-1]
 
     if name_type == '':
-        print("输入类型示例:doc,docx,xls,xlsx,jpg等")
-        name_type = '.' + input("请输入类型: ")
-        name += name_type
+        messagebox.showwarning('错误', '缺少文件类型,示例:doc,docx,xls,xlsx,jpg');
+        return ''
+
     if name.find('%d') == -1:
         name = name.replace(name_type, '-%d' + name_type)
 
@@ -39,7 +39,7 @@ def batch_rename(path, name):
             os.rename(path + '/' + file, path + '/' + file_new)
             message += "\"%s\" 改名为 \"%s\" | " % (file, file_new)
             num += 1
-    return message,num
+    return message, num
 
 
 # 第1步，实例化object，建立窗口window
@@ -47,6 +47,7 @@ window = tk.Tk()
 
 # 第2步，给窗口的可视化起名字
 window.title('批量改名')
+
 
 def set_win_center(root, curWidth=200, curHight=200):
     '''
@@ -77,12 +78,13 @@ def set_win_center(root, curWidth=200, curHight=200):
     size_xy = '%dx%d+%d+%d' % (curWidth, curHight, cen_x, cen_y)
     root.geometry(size_xy)
 
+
 # 第3步，设定窗口的大小(长 * 宽)
 # window.geometry('500x300')  # 这里的乘是小x
-set_win_center(window,500,250)
+set_win_center(window, 500, 250)
 
 # 第4步，在图形界面上设定标签
-l = tk.Label(window, text='批量改名', bg='#f0f0f0', font=('黑体', 16),height=1)
+l = tk.Label(window, text='批量改名', bg='#f0f0f0', font=('黑体', 16), height=1)
 # 说明： bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
 
 # 第5步，放置标签
@@ -90,32 +92,35 @@ l.pack(fill='x')  # Label内容content区域放置位置，自动调节尺寸
 # 放置lable的方法有：1）l.pack(); 2)l.place();
 
 
-tk.Label(window, text='1.输入需改名文件的目录,不填写为当前目录.', bg='#f0f0f0', font=('黑体', 12)).pack(fill='x',pady=0,ipady=0)
-tk.Label(window, text="2.输入需改为的名称,格式为\"名称-%%d.类型\",%%d代表序号.", bg='#f0f0f0', font=('黑体', 12)).pack(fill='x',pady=0,ipady=0)
+tk.Label(window, text='1.输入需改名文件的目录,不填写为当前目录.', bg='#f0f0f0', font=('黑体', 12)).pack(fill='x', pady=0, ipady=0)
+tk.Label(window, text='2.输入需改为的名称,格式为"名称-%d.类型",%d代表序号.', bg='#f0f0f0', font=('黑体', 12)).pack(fill='x', pady=0, ipady=0)
 
 path_var = tk.StringVar()
 path_var.set(os.getcwd())
-path_entry = ttk.Entry(window, textvariable=path_var, font=('微软雅黑', 14))
+path_entry = ttk.Entry(window, textvariable=path_var, font=('微软雅黑', 12))
 path_entry.pack(ipadx=80)
 
+
 def select_path():
-    path=filedialog.askdirectory()
+    path = filedialog.askdirectory()
     if path != '':
         path_var.set(path)
 
-ttk.Button(window,text='选择文件夹', command=select_path).pack()
+
+ttk.Button(window, text='选择文件夹', command=select_path).pack()
 
 name_var = tk.StringVar()
 name_var.set('file-%d.txt')
-name_entry = ttk.Entry(window, textvariable=name_var, font=('微软雅黑', 14))
+name_entry = ttk.Entry(window, textvariable=name_var, font=('微软雅黑', 12))
 name_entry.pack(ipadx=80)
 
 
 def begin_handle():
-    messages,num = batch_rename(path_var.get(),name_var.get())
-    messagebox.showinfo('提示',"修改%d个文件名称"%(num,))
-ttk.Button(window,text='批量修改', command=begin_handle).pack()
+    messages, num = batch_rename(path_var.get(), name_var.get())
+    messagebox.showinfo('提示', "修改%d个文件名称" % (num - 1,))
 
+
+ttk.Button(window, text='批量修改', command=begin_handle).pack()
 
 
 def callback():
@@ -140,7 +145,6 @@ menubar.add_cascade(label="关于", menu=editmenu)
 
 # 显示菜单
 window.config(menu=menubar)
-
 
 # 第6步，主窗口循环显示
 window.mainloop()
